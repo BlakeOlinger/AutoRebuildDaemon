197f4726f25f1817bd023a2c5c68775770c6b354
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-14 12:59:39 -0400

    -- tmp

--- AutoRebuildPart/Program.cs
+++ AutoRebuildPart/Program.cs
@@ -13,22 +13,58 @@ namespace AutoRebuildPart
             
             // rebuild current model
             var model = (ModelDoc2)swInstance.ActiveDoc;
-            // model.ForceRebuild3(true);
+
+            // FIXME - for any write to *.txt config file - write 1 of three states; ! - rebuild as negate - write negate; "" - rebuild write positive/don't overwrite; (-) - rebuild positive write negative
+            //  - this way it's possible to determine if the negation is versus a prior negative state or not -
+            //  - adjust C# to allow for this to happen
 
             // read rebuild.txt file written to by Java GUI
-            /*var rebuildPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
+            var rebuildPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
             var fileContent = System.IO.File.ReadAllLines(rebuildPath);
-            */
-            // read contents of config file
-            // var configContentsLines = System.IO.File.ReadAllLines(@fileContent[0]);
+            var matesToFlip = new string[] { };
+            // creates a new array for the mates that need to be flipped
+            if (fileContent.Length > 1)
+            {
+                matesToFlip = new string[fileContent.Length - 1];
+                for (var i = 1; i < fileContent.Length; ++i)
+                {
+                    matesToFlip[i - 1] = fileContent[i];
+                }
+                // flips the mate if the X/Z offset is negative relative to current position
+                var cutOff = 5_000;
+                var firstFeature = (Feature)model.FirstFeature();
+                while (firstFeature != null && cutOff-- > 0)
+                {
+                    if ("MateGroup" == firstFeature.GetTypeName())
+                    {
+                        var mateGroup = (Feature)firstFeature.GetFirstSubFeature();
+                        var index = 0;
+                        while (mateGroup != null)
+                        {
+                            var mate = (Mate2)mateGroup.GetSpecificFeature2();
+                            var mateName = mateGroup.Name;
+                            foreach (string dimension in matesToFlip)
+                            {
+                                if (dimension == mateName)
+                                {
+                                    Console.WriteLine(mate.Flipped);
+                                    mate.Flipped = !mate.Flipped;
+                                    Console.WriteLine(mate.Flipped);
+                                }
+                            }
 
-            // TODO - if line contains a negative and the rebuild.txt file (fileContent[0]) contains a *.SLDASM
-            // - have that given line's dimension - search out the equation - 'flipped'
-            // - will be necessary to 'flip' them for each feature related to the negative offset
-            // - make sure to only flip the dimension once - maybe I'll simply send a 'should flip' bit for a given dimension
-            // - maybe I'll simply write the dimension to flip in the rebuild.txt file
-            // replace any '-' characters with '' to swap the negative values after the first rebuild
-            /* for(var i = 0; i < configContentsLines.Length; ++i)
+                            mateGroup = (Feature)mateGroup.GetNextSubFeature();
+                            ++index;
+                        }
+                    }
+                    firstFeature = (Feature)firstFeature.GetNextFeature();
+                }
+            }
+            
+            // read contents of config file
+            var configContentsLines = System.IO.File.ReadAllLines(@fileContent[0]);
+
+            for(var i = 0; i < configContentsLines.Length; ++i)
              {
                  if (configContentsLines[i].Contains("-") && configContentsLines[i].Contains("in"))
                  {
@@ -40,40 +76,15 @@ namespace AutoRebuildPart
              {
                  newContent += line + "\n";
              }
-             */
+             
             // write new config contents
-            // System.IO.File.WriteAllText(fileContent[0], newContent);
+            System.IO.File.WriteAllText(fileContent[0], newContent);
 
             // wait a few seconds
-            // Thread.Sleep(2_000);
+            Thread.Sleep(2_000);
 
             // rebuild a second time
             //model.ForceRebuild3(true);
-
-            var cutOff = 500; 
-            var firstFeature = (Feature)model.FirstFeature();
-            while (firstFeature != null && cutOff-- > 0)
-            {
-                if ("MateGroup" == firstFeature.GetTypeName())
-                {
-                    var mateGroup = (Feature)firstFeature.GetFirstSubFeature();
-                    var index = 0;
-                    while (mateGroup != null)
-                    {
-                        var mate = (Mate2)mateGroup.GetSpecificFeature2();
-                        Console.WriteLine(mateGroup.Name + " : " + mate.MateEntity(0).ReferenceComponent.Name);
-                        if (index == 8)
-                        {
-                            // mate.Flipped = !mate.Flipped;
-                            // Thread.Sleep(2_000);
-                           // model.ForceRebuild3(false);
-                        }
-                        mateGroup = (Feature)mateGroup.GetNextSubFeature();
-                        ++index;
-                    }
-                }
-                firstFeature = (Feature)firstFeature.GetNextFeature();
-            }
         }
     }
 }
