6951d2cbd2a635e7b9b002ab40717728f4affb92
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-14 09:39:37 -0400

    -- fixed bug - rebuild would overwrite negation for dimensions - other system processes then were not aware of those negations

--- AutoRebuildPart/Program.cs
+++ AutoRebuildPart/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SldWorks;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace AutoRebuildPart
@@ -12,7 +13,7 @@ namespace AutoRebuildPart
             
             // rebuild current model
             var model = (ModelDoc2)swInstance.ActiveDoc;
-           // model.ForceRebuild3(true);
+            model.ForceRebuild3(true);
             
             // read rebuild.txt file written to by Java GUI
             var rebuildPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
@@ -21,11 +22,15 @@ namespace AutoRebuildPart
             // read contents of config file
             var configContentsLines = System.IO.File.ReadAllLines(@fileContent[0]);
 
+            var hasNegation = false;
+            var linesWithNegation = new Dictionary<string, int>();
             // replace any '-' characters with '' to swap the negative values after the first rebuild
             for(var i = 0; i < configContentsLines.Length; ++i)
             {
                 if (configContentsLines[i].Contains("-") && configContentsLines[i].Contains("in"))
                 {
+                    hasNegation = true;
+                    linesWithNegation.Add(configContentsLines[i], i);
                     configContentsLines[i] = configContentsLines[i].Replace("-", "");
                 }
             }
@@ -43,6 +48,24 @@ namespace AutoRebuildPart
 
             // rebuild a second time
             model.ForceRebuild3(true);
+
+            // replace the negative values so other systems are aware of them
+            if (hasNegation)
+            {
+                var newContentLines = newContent.Split('\n');
+                foreach (string line in linesWithNegation.Keys)
+                {
+                    newContentLines[linesWithNegation[line]] = line;
+                }
+
+                var newContentWithNegation = "";
+                foreach (string line in newContentLines)
+                {
+                    newContentWithNegation += line + "\n";
+                }
+
+                System.IO.File.WriteAllText(fileContent[0], newContentWithNegation);
+            }
         }
     }
 }
