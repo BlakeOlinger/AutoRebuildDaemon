5a84328331b73e042a9e757c08b46bfa6ced5fcc
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-14 15:29:14 -0400

    -- EOD

--- AutoRebuildPart/Program.cs
+++ AutoRebuildPart/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SldWorks;
+using SwConst;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -17,6 +18,8 @@ namespace AutoRebuildPart
             // FIXME - for any write to *.txt config file - write 1 of three states; ! - rebuild as negate - write negate; "" - rebuild write positive/don't overwrite; (-) - rebuild positive write negative
             //  - this way it's possible to determine if the negation is versus a prior negative state or not -
             //  - adjust C# to allow for this to happen
+
+            // TODO - allow user to set material via reading a variable in the config.txt file
 
             // read rebuild.txt file written to by Java GUI
             var rebuildPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
@@ -76,12 +79,21 @@ namespace AutoRebuildPart
              {
                  newContent += line + "\n";
              }
-             
+
+            // allow user to select material via a variable in config.txt file for *.SLDPRT files - intended for covers only atm
+            var partDoc = (PartDoc)model;
+            var materialDatabases = swInstance.GetMaterialDatabases();
+            var bodies = partDoc.GetBodies2((int)swBodyType_e.swAllBodies, false);
+            var db = (string)materialDatabases[3];
+            // read config.txt file and get variable for material - run through switch and set if different
+            var propertyName = "";
+            partDoc.SetMaterialPropertyName(db, "ASTM A36 Steel");
+
             // write new config contents
-            System.IO.File.WriteAllText(fileContent[0], newContent);
+            // System.IO.File.WriteAllText(fileContent[0], newContent);
 
             // wait a few seconds
-            Thread.Sleep(2_000);
+            // Thread.Sleep(2_000);
 
             // rebuild a second time
             //model.ForceRebuild3(true);
