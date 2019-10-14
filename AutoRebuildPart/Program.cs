943cc12d7a1de7ed066e5dec533f3dfb7d242319
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-14 10:15:17 -0400

    -- bug fix - in trying to keep track of negation versus previous configurations while allowing that state to persist beyond the rebuild process introduced a bug where if negation was used the quadrant of the feature would flip flop on across the negative/positive axis on each rebuild

--- AutoRebuildPart/Program.cs
+++ AutoRebuildPart/Program.cs
@@ -22,15 +22,11 @@ namespace AutoRebuildPart
             // read contents of config file
             var configContentsLines = System.IO.File.ReadAllLines(@fileContent[0]);
 
-            var hasNegation = false;
-            var linesWithNegation = new Dictionary<string, int>();
             // replace any '-' characters with '' to swap the negative values after the first rebuild
             for(var i = 0; i < configContentsLines.Length; ++i)
             {
                 if (configContentsLines[i].Contains("-") && configContentsLines[i].Contains("in"))
                 {
-                    hasNegation = true;
-                    linesWithNegation.Add(configContentsLines[i], i);
                     configContentsLines[i] = configContentsLines[i].Replace("-", "");
                 }
             }
@@ -48,24 +44,6 @@ namespace AutoRebuildPart
 
             // rebuild a second time
             model.ForceRebuild3(true);
-
-            // replace the negative values so other systems are aware of them
-            if (hasNegation)
-            {
-                var newContentLines = newContent.Split('\n');
-                foreach (string line in linesWithNegation.Keys)
-                {
-                    newContentLines[linesWithNegation[line]] = line;
-                }
-
-                var newContentWithNegation = "";
-                foreach (string line in newContentLines)
-                {
-                    newContentWithNegation += line + "\n";
-                }
-
-                System.IO.File.WriteAllText(fileContent[0], newContentWithNegation);
-            }
         }
     }
 }
